97f571f49b096ac4cd12a0716a712c17d6153799
chatzyr <146387929+chatzyr@users.noreply.github.com>
2024-01-24 22:25:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,30 @@ app.use(bodyParser.json());
 
 // GET route for /listen
 app.get('/listen', (req, res) => {
-  console.log('Received GET request on /listen');
-  res.send('GET request received');
+    console.log('Received GET request ');
+
+   const queryParam = req.query.queryParam; // assuming your query parameter is named 'queryParam'
+   console.log('Query Prams '+queryParam);
+  // Make sure a query parameter is provided
+  // if (!queryParam) {
+  //   return res.status(400).json({ error: 'Query parameter is required' });
+  // }
+// try {
+//     // Define the URL to which you want to send the POST request
+//     const apiUrl = queryParam; // Replace with your desired URL
+    
+//     // Send the POST request using Axios
+//     const response = await axios.post(apiUrl, { queryParam });
+
+//     // Send the JSON response from the other API to the client
+//     res.json(response.data);
+//   } catch (error) {
+//     // Handle any errors that may occur during the request
+//     console.error('Error making Axios POST request:', error.message);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+  
+
 });
 
 // POST route for /listen
