d7a1c3e101a7ca541e1866bb4250f8d024a8e199
Tahir Saeed <tahirsaeedt7009@gmail.com>
2024-01-29 15:48:15 +0500

    all ips test

--- index.js
+++ index.js
@@ -6,44 +6,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 const ping = require("ping");
 
-async function getPublicIP() {
-  try {
-    const response = await axios.get("https://api64.ipify.org?format=json");
-    const ip = response.data.ip;
-    console.log("Your public IP address is:", ip);
-  } catch (error) {
-    console.error("Error fetching public IP:", error.message);
-  }
-}
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
 // GET route for /listen
 app.get("/listen", async (req, res) => {
-  console.log("Received GET requestt ");
-
-  await getPublicIP();
-  res.send("TAHIR");
-  // const queryParam = req.query.queryParam; // assuming your query parameter is named 'queryParam'
-  // console.log('Query Pramsss '+queryParam);
-  // Make sure a query parameter is provided
-  // if (!queryParam) {
-  //   return res.status(400).json({ error: 'Query parameter is required' });
-  // }
-  // try {
-  //     // Define the URL to which you want to send the POST request
-  //     const apiUrl = queryParam; // Replace with your desired URL
-
-  //     // Send the POST request using Axios
-  //     const response = await axios.post(apiUrl, { queryParam });
-
-  //     // Send the JSON response from the other API to the client
-  //     res.json(response.data);
-  //   } catch (error) {
-  //     // Handle any errors that may occur during the request
-  //     console.error('Error making Axios POST request:', error.message);
-  //     res.status(500).json({ error: 'Internal Server Error' });
-  //   }
+  console.log("Received GET requestt from Alpha Server!!! ");
+  res.send("I am listening ");
+  // await getPublicIP();
 });
 
 // POST route for /listen
@@ -52,16 +22,27 @@ app.post("/listen", (req, res) => {
   console.log("Request body:", req.body);
   res.send("POST request received");
 });
-app.get("/pinged", async (req, res) => {
-  const targetServer = "103.232.255.91"; // Change this to the IP address you want to ping
-  console.log("OK");
-  // Ping the target server
-  ping.sys.probe(targetServer, (isAlive) => {
-    const status = isAlive ? "alive" : "dead";
-    console.log(`Target server (${targetServer}) is ${status}`);
-    res.json({ status });
-  });
+const targetServers = [
+  "103.232.255.91",
+  "103.232.255.90",
+  "103.232.255.92",
+  "202.59.254.101",
+];
+
+app.get("/ping", async (req, res) => {
+  const pingResults = await Promise.all(
+    targetServers.map(async (targetServer) => {
+      // Ping the target server
+      const isAlive = await ping.promise.probe(targetServer);
+      const status = isAlive ? "alive" : "dead";
+      return { targetServer, status };
+    })
+  );
+
+  console.log("Ping results:", pingResults);
+  res.json(pingResults);
 });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
---
+++ test.js
@@ -0,0 +1,9 @@
+// async function getPublicIP() {
+//   try {
+//     const response = await axios.get("https://api64.ipify.org?format=json");
+//     const ip = response.data.ip;
+//     console.log("Your public IP address is:", ip);
+//   } catch (error) {
+//     console.error("Error fetching public IP:", error.message);
+//   }
+// }
